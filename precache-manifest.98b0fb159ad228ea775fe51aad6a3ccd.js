0292dc888e8311eda949e42d130c7f3c19ab7291
Rhythm Bansal <rskbansal@outlook.com>
2022-02-11 01:25:11 +0530

    Updates

---
+++ precache-manifest.98b0fb159ad228ea775fe51aad6a3ccd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb9cfb0b8e13a57a066eed5aa44f679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a61695924b55c880bc71",
+    "url": "/static/css/main.cc5bb653.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61695924b55c880bc71",
+    "url": "/static/js/main.441a1523.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "a09d0231f8f96875e8cd09bee5eabe36",
+    "url": "/static/media/profile.a09d0231.jpeg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
